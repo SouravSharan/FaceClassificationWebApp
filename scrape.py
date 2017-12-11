7c6d8437574b1996994e74f5153f4ec2f4d7b178
Sourav Sharan <souravthesharan@gmail.com>
2017-12-11 22:43:29 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -10,20 +10,19 @@ def get_soup(url,header):
     return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
 
 
-query = input("query image")# you can change the query for the image  here
+query = input("query image")
 image_type="ActiOn"
 query= query.split()
 query='+'.join(query)
 url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
 print(url)
-#add the directory for your image here
-DIR="images"
+
 header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
 }
 soup = get_soup(url,header)
 
 
-ActualImages=[]# contains the link for Large original images, type of  image
+ActualImages=[]
 for a in soup.find_all("div",{"class":"rg_meta"}):
     link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
     ActualImages.append((link,Type))
@@ -36,16 +35,12 @@ DIR = os.path.join(DIR, query.split()[0])
 
 if not os.path.exists(DIR):
             os.mkdir(DIR)
-###print images
 
 counter = 100
 
 for i , (img , Type) in enumerate( ActualImages):
     try:
-        '''
-        req = urllib.request.Request(img, headers={'User-Agent' : header})
-        raw_img = urllib.request.urlopen(req).read()
-        '''
+       
         counter += 1
         req = urllib.request.Request(img, headers=header)
         raw_img = urllib.request.urlopen(req, None, 15).read()
